baa12d830e8d6be73707e6ae4e7937893273d438
Konsgio <rnd-praktiki04@olympia-electronics.com>
2021-11-05 16:04:04 +0200

    working css -> next mobile settings

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import App from './App.vue'
 import router from './router'
 import BootstrapVue from "bootstrap-vue"
 import "bootstrap/dist/css/bootstrap.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
-
+import "./components/home/home.css"
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
