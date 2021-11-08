6df5c36514d60ccc9317efcf81857e79e308550c
Konsgio <rnd-praktiki04@olympia-electronics.com>
2021-11-08 09:37:18 +0200

    added vuex

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -17,11 +17,11 @@
            </svg>
         </button>
     </div>
-        <button type="button" class="btn"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+        <router-link to="/login" type="button" class="btn"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
           <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
           </svg>
-        </button>
+        </router-link>
   <router-view/>
     <div class="back">
       <ul class="side-menu">
@@ -223,6 +223,17 @@ export default {
         panel:[]
     }
     },
+    computed: {
+        user () {
+            return this.$store.state.authentication.user;
+        },
+        users () {
+            return this.$store.state.users.all;
+        }
+    },
+    created () {
+        this.$store.dispatch('users/getAll');
+    },
     mounted() {
       this.polling = setInterval(() => {
       fetch("http://10.0.9.126/api/v1/lum/panel/status")
@@ -240,6 +251,6 @@ export default {
 </script>
 
 <style lang="scss">
-  @import './home.css';
+
   
 </style>
