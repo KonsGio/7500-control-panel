0c2ae34f100ee74afeb0b7e235678c575800c745
Konsgio <konstantinos.giovanitsas@yahoo.com>
2021-11-16 14:48:35 +0200

    tech

--- src/components/technician/technician.vue
+++ src/components/technician/technician.vue
@@ -60,24 +60,7 @@
               <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0Z"/>
              </svg>
          </li>
-         <ul class="indicators-menu">
-        <li>PANEL STATUS</li>
-        <div >
-              <span v-if="data.panel_state===2" class="led1">
-                </span>
-              <span v-else class="led1off">
-              </span>
-        </div>
         
-          <span class="ind-line"></span>
-        <li>LUMINARIES STATUS</li>
-        <div >
-              <span v-if="data.luminaire_state ===2" class="led2">
-                </span>
-              <span v-else class="led2off">    
-              </span>
-        </div>
-      </ul>
       </ul>
       <ul class="mobile-ind-menu">
         <li v-if="data.panel_state ===2" ><svg xmlns="http://www.w3.org/2000/svg" width="18" stroke="green"
